e742ae0f931b3e11fb67446e633276c7ebd8b732
Ht <htcvhs971@gmail.com>
2023-02-16 11:38:17 +0100

    Add class to form Add form declaration

--- script.js
+++ script.js
@@ -7,12 +7,43 @@
 let identifiant = document.getElementById('identifiant');
 
 let field = document.querySelectorAll('.field');
-
+/* 
 identifiant.addEventListener('input', (e) => {
 	if (e.target.value !== '' || e.target.value !== null) {
 		e.preventDefault();
 		field[0].innerHTML += `<p>Il est nécessaire de rentrer un identifiant</p>`;
 	}
-});
+}); */
 //OU
-/* const validIdentifiant = () => {} */
+//?============Déclaration FORM-------START
+(function () {
+	'use strict';
+
+	// Fetch all the forms we want to apply custom Bootstrap validation styles to
+	let forms = document.querySelector('.form-validation');
+
+	(function (validateForm) {
+		validateForm.addEventListener(
+			'submit',
+			function (event) {
+				Array.from(forms.elements).forEach((input) => {
+					if (input.type !== 'submit') {
+						if (!validateFiels(input)) {
+							event.preventDefault();
+							event.stopPropagation();
+
+							input.classList.remove('is-valid');
+							input.classList.add('is-invalid');
+						} else {
+							input.classList.remove('is-invalid');
+							input.classList.add('is-valid');
+						}
+					}
+				});
+			},
+			false
+		);
+	});
+})();
+//?============Déclaration FORM-------END
+// const validIdentifiant = () => {};
